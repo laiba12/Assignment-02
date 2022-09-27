e4d698f1573744ed562dfc41041a94b5421061b0
laiba12 <laiba.noor312@gmail.com>
2022-09-27 16:22:44 +0530

    Add files via upload

---
+++ Q-12.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    float a;
+    printf("Enter amount in rupees=");
+    scanf("%f",&a);
+    a=a/76.23;
+    printf("Amount in USD is= %f",a);
+    return 0;
+
+}
